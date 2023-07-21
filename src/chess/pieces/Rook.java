98911cea0b47add1f7d993f7f34d55cc7d512167
Matheus Francisco <matheusdabmlj@gmail.com>
2023-07-21 13:36:54 -0300

    Possible moves of a piece

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -12,4 +12,8 @@ public class Rook extends ChessPiece {
     public String toString(){
         return "R";
     }
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
+    }
 }
